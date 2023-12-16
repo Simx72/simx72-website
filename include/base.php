c6c9291ec5ef0102922c7cbcddc5301796a00ff1
Simx72 <angel2600@proton.me>
2023-12-16 17:53:29 -0500

    new header

--- include/base.php
+++ include/base.php
@@ -90,15 +90,81 @@ function HeaderR() {
 function myheader()
 {
 	?>
-
+	<nav
+		class="navbar navbar-expand-sm navbar-light bg-light"
+	>
+		<div class="container">
+			<a class="navbar-brand" href="/">Navbar</a>
+			<button
+				class="navbar-toggler d-lg-none"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#top-header-collapsibleNavId"
+				aria-controls="top-header-collapsibleNavId"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="top-header-collapsibleNavId">
+				<ul class="navbar-nav me-auto mt-2 mt-lg-0">
+					<li class="nav-item">
+						<a class="nav-link active" href="/" aria-current="page"
+							>Inicio
+							<span class="visually-hidden">(current)</span></a
+						>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/static">Static</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a
+							class="nav-link dropdown-toggle"
+							href="#"
+							id="dropdownId"
+							data-bs-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false"
+							>Dropdown</a
+						>
+						<div
+							class="dropdown-menu"
+							aria-labelledby="dropdownId"
+						>
+							<a class="dropdown-item" href="#"
+								>Action 1</a
+							>
+							<a class="dropdown-item" href="#"
+								>Action 2</a
+							>
+						</div>
+					</li>
+				</ul>
+				<form class="d-flex my-2 my-lg-0">
+					<input
+						class="form-control me-sm-2"
+						type="text"
+						placeholder="Bus"
+					/>
+					<button
+						class="btn btn-outline-success my-2 my-sm-0"
+						type="submit"
+					>
+						[lupita de detective :D]
+					</button>
+				</form>
+			</div>
+		</div>
+	</nav>
+	
 	<header id="header">
-		<div class="left">
+		<div class="mr-auto">
 			<?php HeaderL(); ?>
 		</div>
 		<div class="center">
 			<?php HeaderC(); ?>
 		</div>
-		<div class="right">
+		<div class="ml-auto">
 			<?php HeaderR(); ?>
 		</div>
 	</header>
