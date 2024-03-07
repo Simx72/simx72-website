434ceb4b3752550dfe77b34ae984ec87fc819a90
Simx72 <angel2600@proton.me>
2024-03-07 16:39:20 -0500

    alerta para cuando la conexion es no cifrada https-warning verificar_conexion

--- include/auth.php
+++ include/auth.php
@@ -30,25 +30,25 @@ require_once __DIR__ . '/../include/mailer.php';
 $auth = new \Delight\Auth\Auth($db);
 
 $challenge_available_algorithms = array(
-    "SHA-256" => "sha256",
-    "SHA-384" => "sha384",
-    "SHA-512" => "sha512"
+	"SHA-256" => "sha256",
+	"SHA-384" => "sha384",
+	"SHA-512" => "sha512"
 );
 
 function crear_challenge(string|null $salt, int|null $secret_number)
 {
-    global $env, $challenge_available_algorithms;
+	global $env, $challenge_available_algorithms;
 
-    $salt = $salt ?? bin2hex(random_bytes(12));
-    $secret_number = $secret_number ?? random_int(100_000, 350_000);
+	$salt = $salt ?? bin2hex(random_bytes(12));
+	$secret_number = $secret_number ?? random_int(100_000, 350_000);
 
-    $algorithm = $challenge_available_algorithms[$env['ALTCHA_ALGORITHM']];
+	$algorithm = $challenge_available_algorithms[$env['ALTCHA_ALGORITHM']];
 
-    $challenge = hash($algorithm, $salt . $secret_number);
+	$challenge = hash($algorithm, $salt . $secret_number);
 
-    $signature = hash_hmac($algorithm, $challenge, $env['ALTCHA_HMAC_KEY']);
+	$signature = hash_hmac($algorithm, $challenge, $env['ALTCHA_HMAC_KEY']);
 
-    return ['algorithm' => $env['ALTCHA_ALGORITHM'], 'challenge' => $challenge, 'signature' => $signature, 'salt' => $salt];
+	return ['algorithm' => $env['ALTCHA_ALGORITHM'], 'challenge' => $challenge, 'signature' => $signature, 'salt' => $salt];
 }
 
 function altcha_es_valido(string $altcha): bool
@@ -65,4 +65,42 @@ function altcha_es_valido(string $altcha): bool
 			&& $json['signature'] === $check['signature'];
 	}
 	return false;
+}
+
+
+function verificar_conexion()
+{
+	if (
+		!(isset($_SERVER['HTTPS']) &&
+			($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+			isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+			$_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+	) {
+		?>
+		<div class="alert alert-danger" role="alert" id="https-warning">
+			<div>
+				<h3>Cuidado</h3>
+			</div>
+			<div>
+				La conexión no esta protegida por el protocolo HTTPS. La información podría ser robada en tránsito
+			</div>
+		</div>
+		<script>
+			{
+				let btn = document.createElement('button');
+				btn.type = "button";
+				btn.classList.add("btn", "btn-success");
+				btn.innerHTML = 'Proteger <i class="bi bi-lock"></i>';
+				let secureUrl = new URL(window.location.href);
+				secureUrl.protocol = "https";
+				btn.addEventListener('click', _ => {
+					window.location.url = secureUrl;
+				})
+				let div = document.createElement('div');
+				div.appendChild(btn);
+				document.getElementById('https-warning').appendChild(div);
+			}
+		</script>
+		<?php
+	}
 }
